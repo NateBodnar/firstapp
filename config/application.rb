d90682f84cdbd1298328c107c6f318d1ba2d346b
Nathaniel Bodnar <connmoviemakers@gmail.com>
2013-03-12 14:51:56 -0400

    Added devise,Signup,Login,Logout

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Firstapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Required Devise on Heroku
+    config.assets.initializer_on_precompile = false
   end
 end
