96ddce4f78f4cd6e5383aa0e2121319071ed9bf5
Nathaniel Bodnar <connmoviemakers@gmail.com>
2013-03-19 10:57:07 -0400

    Add Projects Association

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   attr_accessible :desvription
 
   validates :desvription, presence: true
+
+  belongs_to :user
+
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315181748) do
+ActiveRecord::Schema.define(:version => 20130319142015) do
 
   create_table "projects", :force => true do |t|
     t.string   "desvription"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
+
+  add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
