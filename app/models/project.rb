56b7d4fdebc8573c0f89891b89638effa367e312
Nathaniel Bodnar <connmoviemakers@gmail.com>
2013-03-19 13:41:24 -0400

    Added Paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,15 @@
 class Project < ActiveRecord::Base
-  attr_accessible :desvription
+  attr_accessible :desvription, :image
+
+
 
   validates :desvription, presence: true
+	validates :user_id, presence: true
+	validates_attachment :image, presence: true,
+															 content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+															 size: { less_than: 5.megabytes }
 
   belongs_to :user
-
-  validates :user_id, presence: true
+  has_attached_file :image, styles: { medium: "320x240>" }
+  
 end
